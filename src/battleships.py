5b72a363bd5d9e6883e983ac2310a80ef9fcdb19
Matthew Nickson <mnickson@sidingsmedia.com>
2021-02-23 15:45:50 +0000

    Added docs and game class
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- src/battleships.py
+++ src/battleships.py
@@ -10,20 +10,36 @@ https://raw.githubusercontent.com/Computroniks/python-battleships/main/LICENSE
 
 #Define custom exceptions
 class Error(Exception):
-    """Base class for other exceptions"""
+    """Base class for other exceptions
     
-    pass
+    This is the base class on which all custom errors are based.
+
+    Attributes
+    ----------
+    message : str, optional
+        An explanation of the error
+    """
+    
+    def __init__(self, message:str="An unexpected error occured") -> None:
+        self.message = message
+        super().__init__(self.message)
+
 class PositionAlreadyPopulated(Error):
     """Raised when position is already populated
     
     This error is raised when a ship is trying to be placed in
     a position which is already populated by another ship.
-
+sss
     Attributes
     ----------
+    message : str, optional
+        An explanation of the error
     """
 
-    pass
+    def __init__(self, message:str="This position is already populated") -> None:
+        self.message:str = message
+        super().__init__(self.message)
+
 class Board():
     """A class that handles anything to do with the game board
 
@@ -53,16 +69,35 @@ class Board():
         """
 
         self.map:list = [[0 for i in range(x)] for j in range(y)]
-    def addShip(self, size:int=5, posX:int=0, posY:int=0) -> bool:
+    
+    def addShip(self, size:int, posX:int, posY:int, rotDir:bool=False) -> None:
         """Adds a ship of specified size to board starting at specified coordinates
 
         Parameters
         ----------
-        size
+        size : int
+            The size of the ship.
+        posX : int
+            The x coordinate for the start of the ship
+        posY : int
+            The y coordinate for the start of the ship
+        rotDir : bool, optional
+            The direction of the ship. True is vertical. False is horizontal.
+            (default False)
+
+        Raises
+        ------
+        PositionAlreadyPopulated
+            If position for ship is already taken.
         """
 
         pass
+
     def printBoard(self) -> None:
+        """Prints the game board
+
+        This function prints out the gameboard with all items seen
+        """
         #Print x heading
         print(f"\n\n|{' ':^3}|", end='')
         for i in range(len(self.map[0])):
@@ -72,7 +107,15 @@ class Board():
             print(f'\n|{i+1:^3}|', end='')
             for j in range(len(self.map[i])):
                 print(f'{self.map[i][j]:^3}|', end='')
+
+
     def printBoardHidden(self) -> None:
+        """Prints the game board
+
+        This function prints out the gameboard but all items except for hits and 
+        misses are redacted.
+        """
+
         #Print x heading
         print(f"\n\n|{' ':^3}|", end='')
         for i in range(len(self.map[0])):
@@ -85,6 +128,53 @@ class Board():
                     print(f'{self.map[i][j]:^3}|', end='')
                 else:
                     print(f"{'#':^3}|", end='')
+
+
+class Game():
+    """This class handles the gameplay and controls all aspects of the game
+
+    Attributes
+    ----------
+    map : list
+        a 2d list that is the game board
+
+    Methods
+    -------
+    addShip(size, posX, posY)
+        Adds a ship of size `size` starting at `posX` `posY`
+    printBoard()
+        Prints the game board
+    printBoardHidden()
+        Prints the gameboard but hides all except hits and misses
+    """
+    def __init__(self):
+        while True:
+            try:
+                self.width:int = int(input('Please enter the board width: '))
+                if (self.width < 1):
+                    print('The minimum board width is 1')
+                    continue
+                elif (int(str(abs(self.width))) > 3):
+                    print('The maximum board width is 999')
+                    continue
+                else:
+                    break
+            except ValueError:
+                print('Please enter a valid number!')
+        while True:
+            try:
+                self.height:int = int(input('Please enter the board height: '))
+                if (self.height < 1):
+                    print('The minimum board height is 1')
+                    continue
+                elif (int(str(abs(self.height))) > 3):
+                    print('The maximum board height is 999')
+                    continue
+                else:
+                    break
+            except ValueError:
+                print('Please enter a valid number!')
+        self.gameboard = Board(self.width, self.height)
 #Main game loop
 if __name__ == '__main__':
     gameBoard = Board(10,10)
